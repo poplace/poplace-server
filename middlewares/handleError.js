3c4d8cc7bb462e0eab393d57020ac847a4757a5c
Onea <onea8906@gmail.com>
2021-09-09 19:22:41 +0900

    refactor: users/login logic

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,7 +1,6 @@
 function handleError(err, req, res, next) {
-  console.log(err);
-
   res.status(err.status || 500);
+
   res.json({
     code: err.status,
     message: err.message ? err.message : "Internal server error",
