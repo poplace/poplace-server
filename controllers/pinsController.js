4ebffffd4fcb27eae3b3629e4ed7385503e25f50
Onea <onea8906@gmail.com>
2021-09-13 01:39:48 +0900

    feat: apply get pins in my page

--- controllers/pinsController.js
+++ controllers/pinsController.js
@@ -5,7 +5,6 @@ const Pin = require("../models/Pin");
 const User = require("../models/User");
 const ERROR = require("../constants/error");
 
-const SECRET_KEY = process.env.SECRET_KEY;
 const AWS_REGION = process.env.AWS_REGION;
 const IDENTITY_POOL_ID = process.env.IDENTITY_POOL_ID;
 const AWS_S3_BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
@@ -17,18 +16,22 @@ exports.getMyPin = async function (req, res, next) {
     const user = await User.findOne({ email }).lean();
     const { _id } = user;
 
-    const myCreatedPins = await Pin.find({ creator: _id }).lean();
-
-    console.log(myCreatedPins);
-
     if (!user) {
       return next(createError(400, ERROR.notFoundUser));
     }
+
+    const myCreatedPins = await Pin.find({ creator: _id }).lean();
+
+    const pins = myCreatedPins.map((pin) => {
+      const { image, text, tag, createdAt, savedAt } = pin;
+
+      return { image, text, tag, createdAt, savedAt: savedAt || null };
+    });
+
+    return res.json({ status: "OK", pins });
   } catch (err) {
     next(err);
   }
-
-  return res.json({ status: "OK" });
 };
 
 exports.createPin = async function (req, res, next) {
