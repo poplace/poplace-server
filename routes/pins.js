84a692547f46876e46eed7a5f91017d6b292c205
ham-myeongyeon <hmy751@gmail.com>
2021-09-13 17:22:08 +0900

    refactor: edit miss code

--- routes/pins.js
+++ routes/pins.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
 
 const pinsController = require("../controllers/pinsController");
-
 const verifyToken = require("../middlewares/verifyToken");
 
+router.get("/", pinsController.getMyPin);
 router.put("/:pinId", verifyToken, pinsController.updatePin);
-router.post("/", verifyToken, pinsController.createPin);
+router.post("/", verifyToken, upload.fields([{ name: "photo" }]), pinsController.createPin);
 
 module.exports = router;
